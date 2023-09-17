818d4480dfac66468879ba878c3aeaf5cc11d266
David Bowles <david.bowles84272@outlook.com>
2023-09-17 14:22:20 +0000

    Add tasks and display

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,5 +2,21 @@ document.addEventListener('DOMContentLoaded', function() {
     // sidenav initialisation
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+
+    // datepicker initialization, cahen var to let and our name
+    // remove the placeholder options into an object found in materialse options
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+      format: "dd mmm, yyyy",
+      i18n: {done: "Select"}
+    });
+
+    // select/dropdown initialization
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
+
+    // collapsible initialization
+    let collapsible = document.querySelectorAll('.collapsible');
+    M.Collapsible.init(collapsible);
   });
   
